a4c8580b992b1b7c0a95b43cb2474a9d403e4d75
子幸 <93656339+deng-goods@users.noreply.github.com>
2024-12-28 12:47:00 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 let userID = '';
 let proxyIP = '';
-let sub = 'VLESS.fxxk.dedyn.io';
+let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
 let subProtocol = 'https';
